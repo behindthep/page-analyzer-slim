4468ace554a53715acd993213a590382721cd22d
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-04-20 18:17:46 +0300

    chore: clean test file

---
+++ tests/smthTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Page\Analyzer\Tests;
+
+use PHPUnit\Framework\TestCase;
+// use Page\Analyzer\smth;
+
+class smthTest extends TestCase
+{
+    public function testFun(): void
+    {
+    }
+}
