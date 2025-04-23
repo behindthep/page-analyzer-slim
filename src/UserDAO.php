d11b5b0d10f2c2583eb1ba58d6eee10d53721031
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-04-23 16:28:30 +0300

    fix: fix sonarcloud errors

--- src/UserDAO.php
+++ src/UserDAO.php
@@ -11,7 +11,7 @@ class UserDAO
         try {
             $dsn = "pgsql:host=localhost;port=5432;dbname=practice1;";
             $user = "alex";
-            $password = "";
+            $password = "pass";
             $this->pdo = new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
             $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         } catch (\PDOException $e) {
--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 
 namespace Page\Analyzer;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $dao = new UserDAO();
 
