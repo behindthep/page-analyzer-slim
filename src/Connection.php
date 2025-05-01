209c6658aa6410340ace39bf0f4fbf9303300703
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-01 23:20:30 +0300

    add tables inicialization

--- src/Connection.php
+++ src/Connection.php
@@ -11,16 +11,36 @@ final class Connection
         $databaseUrl = $_ENV['DATABASE_URL'];
         $parsedDbUrl = parse_url($databaseUrl);
 
-        $host     = $parsedUrl['host'] ?? '';
-        $port     = $parsedUrl['port'] ?? '5432';
+        $host     = $parsedDbUrl['host'] ?? '';
+        $port     = $parsedDbUrl['port'] ?? '5432';
         $dbname   = ltrim($parsedDbUrl['path'], '/');
-        $username = $parsedUrl['user'] ?? '';
-        $password = $parsedUrl['pass'] ?? '';
+        $username = $parsedDbUrl['user'] ?? '';
+        $password = $parsedDbUrl['pass'] ?? '';
 
         $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
         $this->pdo = new \PDO($dsn, $username, $password);
         $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+
+        $this->initializeDatabase();
+    }
+
+    private function initializeDatabase(): void
+    {
+        $sqlFilePath = __DIR__ . '/../database.sql';
+
+        if (!file_exists($sqlFilePath)) {
+            throw new \RuntimeException("SQL file not found: $sqlFilePath");
+        }
+
+        $sql = file_get_contents($sqlFilePath);
+
+        try {
+            $this->pdo->exec($sql);
+        } catch (\PDOException $e) {
+            error_log("Ошибка создания таблиц: " . $e->getMessage());
+            throw new \RuntimeException("Ошибка инициализации базы данных.");
+        }
     }
 
     public function get(): \PDO
